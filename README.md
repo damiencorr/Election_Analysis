a2a2801a39166d15bf6e67855332fe144e2fa57e
damiencorr <damien.corr@gmail.com>
2020-06-13 15:55:18 -0400

    test commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Election_Analysis
+Damien Corr - https://github.com/damiencorr/Election_Analysis
--- election_analysis.py
+++ election_analysis.py
@@ -7,3 +7,8 @@
 # The winner of the election based on popular vote
 
 
+""" Open the data file.
+Write down the names of all the candidates.
+Add a vote count for each candidate.
+Get the total votes for each candidate.
+Get the total votes cast for the election. """
